868f7b17917c6722e198853982e5656d89aa917d
wanazmi69 <lawtheoscar@gmail.com>
2022-12-05 18:14:01 +0900

    make static database

--- myweb/views.py
+++ myweb/views.py
@@ -8,15 +8,27 @@ from mypesan.models import Post
 def index(request):
   
 
+  pesanku = [
+    {
+      'name': 'Anonymouse',
+      'addres': 'Indonesia',
+      'messages': 'Hapy good day',
+    },
+    {
+      'name': 'Anonymouse2',
+      'addres': 'Japan',
+      'messages': 'Hapy good day',
+    },
 
-  pesanku = Post.objects.all()
-  if request.method == 'POST':
-    Post.objects.create(
-      nama = request.POST.get('Nama'),
-      alamat = request.POST.get('Alamat'),
-      pesan = request.POST.get('Pesan'),
-    )
-    return HttpResponseRedirect('/#pesanku')
+  ]
+  # pesanku = Post.objects.all()
+  # if request.method == 'POST':
+  #   Post.objects.create(
+  #     nama = request.POST.get('Nama'),
+  #     alamat = request.POST.get('Alamat'),
+  #     pesan = request.POST.get('Pesan'),
+  #   )
+    # return HttpResponseRedirect('/#pesanku')
 
   context = {
     'pesan' : pesanku,
